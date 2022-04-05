4298cb27498db850118c73f424ccfe671968b0c3
GuessWhatBBQ <saminslm@gmail.com>
2022-04-05 14:30:10 +0600

    removed old ui relics

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,30 +1,43 @@
 import React from "react";
-import { Grid, CircularProgress } from "@material-ui/core";
-import Post from "./Post/Post";
 import { useSelector } from "react-redux";
+// import { Grid, CircularProgress } from "@material-ui/core";
+// import Post from "./Post/Post";
 
-import useStyles from "./styles";
+// import useStyles from "./styles";
+
+import FeedContent from "../FeedContent/FeedContent";
 
 function Posts({ setCurrentId }) {
-  const posts = useSelector((state) => state.posts);
-  const classes = useStyles();
+	const posts = useSelector((state) => state.posts);
+	// const classes = useStyles();
 
-  return !posts.length ? (
-    <CircularProgress />
-  ) : (
-    <Grid
-      className={classes.container}
-      container
-      alignItems="stretch"
-      spacing={3}
-    >
-      {posts.map((post) => (
-        <Grid key={post._id} item xs={12} sm={6}>
-          <Post post={post} setCurrentId={setCurrentId} />
-        </Grid>
-      ))}
-    </Grid>
-  );
+	// return !posts.length ? (
+	// 	<CircularProgress />
+	// ) : (
+	// 	<Grid
+	// 		className={classes.container}
+	// 		container
+	// 		alignItems="stretch"
+	// 		spacing={3}
+	// 	>
+	// 		{posts.map((post) => (
+	// 			<Grid key={post._id} item xs={12} sm={6}>
+	// 				<Post post={post} setCurrentId={setCurrentId} />
+	// 			</Grid>
+	// 		))}
+	// 	</Grid>
+	// );
+	return (
+		<>
+			{posts.map((post) => (
+				<FeedContent
+					key={post._id}
+					post={post}
+					setCurrentId={setCurrentId}
+				></FeedContent>
+			))}
+		</>
+	);
 }
 
 export default Posts;
