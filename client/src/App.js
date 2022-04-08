9112671f2e4cbc0523300ee7ccb7b0cbbcc22707
KabirBhae <arafatkabirkhan22@gmail.com>
2022-04-08 23:39:21 +0600

    minor change in homepage styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
-import { Container } from "@material-ui/core";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Auth from "./pages/Auth/Auth";
-import Messenger from './pages/Messenger/Messenger';
+import Messenger from "./pages/Messenger/Messenger";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Container maxWidth="lg">
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/auth" exact element={<Auth />} />
-          <Route path="/messenger" exact element={<Messenger />} >
-            <Route path=":conversationID" element={<Messenger />} />
-          </Route>
-        </Routes>
-      </Container>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="/auth" exact element={<Auth />} />
+				<Route path="/messenger" exact element={<Messenger />}>
+					<Route path=":conversationID" element={<Messenger />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,11 @@ import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 import App from "./App";
-import "./index.css";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
