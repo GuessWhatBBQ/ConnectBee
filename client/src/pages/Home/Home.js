93c121db8a576bbd12b2a19fbccc4bae40d5a2c8
KabirBhae <arafatkabirkhan22@gmail.com>
2022-04-20 10:56:51 +0600

    prod commit 3

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,23 +10,23 @@ import Rightbar from "../../components/Rightbar/Rightbar";
 import "./home.css";
 
 const Home = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const dispatch = useDispatch();
-  const profile = useSelector(state => state.auth);
-  useEffect(() => {
-    if (profile) {
-      dispatch(getPosts());
-    };
-  }, [dispatch, profile]);
-  return (
-    <>
-      <div className="homeContainer">
-        <Leftbar></Leftbar>
-        <Feed currentId={currentId} setCurrentId={setCurrentId}></Feed>
-        <Rightbar></Rightbar>
-      </div>
-    </>
-  );
+	const [currentId, setCurrentId] = useState(null);
+	const dispatch = useDispatch();
+	const profile = useSelector((state) => state.auth);
+	useEffect(() => {
+		if (profile) {
+			dispatch(getPosts());
+		}
+	}, [dispatch, profile]);
+	return (
+		<>
+			<div className="homeContainer">
+				<Leftbar></Leftbar>
+				<Feed currentId={currentId} setCurrentId={setCurrentId}></Feed>
+				{/* <Rightbar></Rightbar> */}
+			</div>
+		</>
+	);
 };
 
 export default Home;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -12,20 +12,19 @@ import { useParams } from "react-router-dom";
 import { createConversation } from "../../api";
 
 export const Profile = () => {
-  const dispatch = useDispatch();
-  const { userId } = useParams();
-  const profile = useSelector(state => state.auth);
-  useEffect(() => {
-    if (userId) {
-      dispatch(getUserPosts(userId));
-    }
-    else if (profile) {
-      dispatch(getUserPosts(profile.authData.result._id));
-    };
-  }, [dispatch, profile, userId]);
-  const handleConversationCreation = () => {
-    createConversation(userId);
-  };
+	const dispatch = useDispatch();
+	const { userId } = useParams();
+	const profile = useSelector((state) => state.auth);
+	useEffect(() => {
+		if (userId) {
+			dispatch(getUserPosts(userId));
+		} else if (profile) {
+			dispatch(getUserPosts(profile.authData.result._id));
+		}
+	}, [dispatch, profile, userId]);
+	const handleConversationCreation = () => {
+		createConversation(userId);
+	};
 	return (
 		<>
 			<div className="profileContainer">
@@ -33,17 +32,23 @@ export const Profile = () => {
 				<div className="profileRight">
 					<div className="profileRightTop">
 						<div className="profileCover">
-							<img src=".2.jpeg" alt="" className="profileCoverImg" />
-							<img src="./1.jpeg" alt="" className="profileUserImg" />
+							<img
+								src="/img/profile/ad.png"
+								alt=""
+								className="profileCoverImg"
+							/>
+							<img src="/img/profile/1.png" alt="" className="profileUserImg" />
 						</div>
 						<div className="profileInfo">
-							<h4 className="profileInfoName">My name is Arafat</h4>
-							<p className="profileInfoDesc">This is description</p>
+							<h4 className="profileInfoName">Sazid</h4>
+							<p className="profileInfoDesc">This is my bio</p>
 						</div>
 					</div>
 					<div className="profileRightBottom">
 						<Feed></Feed>
-            {userId ? <button onClick={handleConversationCreation}>Message</button> : null }
+						{userId ? (
+							<button onClick={handleConversationCreation}>Message</button>
+						) : null}
 					</div>
 				</div>
 			</div>
